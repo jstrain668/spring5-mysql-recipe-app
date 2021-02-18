6f63045f43ecacf47568f3c021d76e9163910ac3
Developer <jjstrain68@gmail.com>
2021-02-18 22:04:39 +0000

    Update dev and prod properties for MySQL

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -18,7 +18,6 @@ import java.util.Optional;
 
 @Slf4j
 @Component
-
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private final CategoryRepository categoryRepository;
