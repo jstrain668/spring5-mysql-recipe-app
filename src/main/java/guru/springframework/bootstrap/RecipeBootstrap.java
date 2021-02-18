0515d172eb38e613b691775cac15cc36ff97b4f4
Developer <jjstrain68@gmail.com>
2021-02-18 22:24:02 +0000

    application-default.properties for h2 database along with data-h2.sql. application-dev.properties and application-prod.properties work with BootStrapMySQL, checks if reference data needs to be loaded or not

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -18,6 +18,7 @@ import java.util.Optional;
 
 @Slf4j
 @Component
+@Profile("default")
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private final CategoryRepository categoryRepository;
